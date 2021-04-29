62e619f9d84dccc641946a86d86a4a68624f4b8d
Victor Parga <victormpargas@gmail.com>
2021-04-29 15:23:44 -0400

    correct port

--- part2.js
+++ part2.js
@@ -1,6 +1,7 @@
+const PORT = process.env.PORT || 3000;
 var http = require('http');
 http.createServer(function (req, res) {  
 	res.writeHead(200, {'Content-Type': 'text/html'});  
 	res.write("Hello World");  
 	res.end();
-}).listen(8080);
+}).listen(PORT);
